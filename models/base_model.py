eeb5c254eb5abbf542e27c603b585f80823a94af
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-02-12 23:42:39 +0000

    added functionality

--- models/base_model.py
+++ models/base_model.py
@@ -7,14 +7,22 @@ from datetime import datetime
 class BaseModel():
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """constructor
         Args:
             none
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if  not len(kwargs) == 0:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                    if "created_at" in kwargs:
+                        self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    if "updated_at" in kwargs:
+                        self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = self.updated_at = datetime.now()
     
     def __str__(self):
         """Attribute method
@@ -22,7 +30,7 @@ class BaseModel():
         Instance Id
         Dict representation of instance attributes
         """
-        return "[{}] ({}) {}".format(self.__class__.__name, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """save
