9987fe0f94883397a87415f52c6da131732441a5
Gichuhi22 <bernardgichuhiwamwea@gmail.com>
2023-02-13 00:11:48 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel():
                 else:
                     self.id = str(uuid.uuid4())
                     self.created_at = self.updated_at = datetime.now()
+                    storage.new(self)
     
     def __str__(self):
         """Attribute method
@@ -37,6 +38,7 @@ class BaseModel():
         Updates attribute Updated_at with current date and time
         """
         self.updated_at = datetime.now
+        storage.save()
 
     def to_dict(self):
         """
